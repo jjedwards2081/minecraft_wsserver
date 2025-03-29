d9fb2f305be9f9b4d85eceb5df945200d407e8ce
Justin Edwards <jnredwards@gmail.com>
2025-03-29 11:36:22 +0000

    Fix run.py to cleanly run using current platform Python

--- run.py
+++ run.py
@@ -2,9 +2,13 @@ import os
 import subprocess
 import platform
 
+venv_folder = ".venv"  # ✅ matches your actual folder name
+
 if platform.system() == "Windows":
-    activate = ".venv\\Scripts\\activate.bat && python server\\main.py"
-    subprocess.call(["cmd.exe", "/c", activate])
+    activate = os.path.join(venv_folder, "Scripts", "activate.bat")
+    command = f"{activate} && python server\\main.py"
+    subprocess.call(["cmd.exe", "/c", command])
 else:
-    activate = "source .venv/bin/activate && python server/main.py"
-    subprocess.call(["bash", "-c", activate])
+    activate = os.path.join(venv_folder, "bin", "activate")
+    command = f"source {activate} && python3 server/main.py"
+    subprocess.call(["bash", "-c", command])
